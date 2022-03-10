558f1cfd00f821c8a63dd07cfbda7f506e8b0318
Lucas Silva <lukasmulekezika2@gmail.comheroku>
2022-03-10 20:17:08 -0300

    Adcionado treinamento e eros e o resultado final da previsao

--- main.py
+++ main.py
@@ -2,18 +2,12 @@
 
 import pandas as pd
 import tensorflow as tf
-
 from tensorflow.keras.layers import Dense, Dropout, Input, LSTM
-from tensorflow.keras.models import Model
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.optimizers import RMSprop
-
 from keras.callbacks import ReduceLROnPlateau
-
 import matplotlib.pyplot as plt
-
 import numpy as np
-
 from sklearn.preprocessing import MinMaxScaler
 
 
