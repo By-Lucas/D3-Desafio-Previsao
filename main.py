0fdb8645eb3af3c7df529c605a9dc1d17a599584
Lucas Silva <lukasmulekezika2@gmail.comheroku>
2022-03-09 23:38:21 -0300

    ADCIONADO VARIAVEIS

--- main.py
+++ main.py
@@ -18,4 +18,25 @@ from sklearn.preprocessing import MinMaxScaler
 
 
 covid_data = pd.read_csv('arquivos/time_series_covid_19_confirmed.csv')
-covid_data.head()
+#covid_data.head()
+
+"""# Preparação de dados
+* Como queremos prever casos para os EUA, extrairemos a linha contendo os casos de covid confirmados nos EUA.
+"""
+
+covid_data = covid_data.loc[covid_data['Country/Region'] == 'Brazil']
+
+#covid_data
+
+"""### Dados de pré-processamento
+* Podemos descartar as colunas para 'Província/Estado', 'País/Região', 'Lat' e 'Long', SE OS  dados são apenas para os EUA e essas colunas não são necessárias para previsão.
+"""
+
+covid_data = covid_data.drop(columns=['Province/State', 'Country/Region', 'Lat', 'Long'])
+
+"""* Como o número de casos de Corona fica bastante grande ao longo do tempo, os cálculos do nosso modelo durante o treinamento podem ser muito lentos. Podemos corrigir isso usando o MinMaxScaler do sklearn para redimensionar nossos dados."""
+
+scaler = MinMaxScaler()
+scaler.fit(covid_data.values.T)
+covid_data = scaler.transform(covid_data.values.T)
+
